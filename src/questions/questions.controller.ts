11d996e24cb24d1d43e5aa1ce6cf2a431a0c4743
AndreasT <andreas.teneketzoglou@medwing.com>
2020-04-13 12:45:40 +0200

    feat: add getAllQuestions service

--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { QuestionsService } from './questions.service';
 
 @Controller('questions')
 export class QuestionsController {
   constructor(private questionsService: QuestionsService) {}
+
+  @Get()
+  getAllQuestions() {
+    this.questionsService.getAllQuestions();
+  }
 }
--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class QuestionsService {}
+export class QuestionsService {
+  private questions = [];
+
+  getAllQuestions() {
+    return this.questions;
+  }
+}
