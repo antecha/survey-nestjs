61b512714b0caa7fb6e271daf180d85f0b785448
Andreas Teneketzoglou <antekai00@gmail.com>
2020-05-10 17:50:39 +0200

    feat: add question entity

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,12 @@
-import {TypeOrmModuleOptions} from '@nestjs/typeorm'
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const typeOrmConfig:TypeOrmModuleOptions = {
-    type:'postgres',
-    host:'localhost',
-    port:5432,
-    username:'postgres',
-    password:'postgres',
-    database:'survey',
-    entities:[__dirname+'/../**/*.entity.ts'],
-    synchronize:true
-} 
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'survey',
+  entities: [__dirname + '/../**/*.entity.ts'],
+  synchronize: true,
+};
---
+++ src/question/question.entity.ts
@@ -0,0 +1,20 @@
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { QuestionType, QuestionOption } from './question.model';
+
+@Entity()
+export class Question extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  surveyID: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  type: QuestionType;
+
+  @Column()
+  options: QuestionOption[];
+}
