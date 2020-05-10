1b5b945124edd9e14d25194a2a98a18a2347404f
Andreas Teneketzoglou <antekai00@gmail.com>
2020-05-10 17:41:12 +0200

    chore: setup postgres and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuestionModule } from './question/question.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [QuestionModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    QuestionModule],
   controllers: [AppController],
   providers: [AppService],
 })
